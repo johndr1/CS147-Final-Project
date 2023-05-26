266fe9166210d00ded3423a11e4a94b883825566
johndr1 <johndr1@uci.edu>
2023-05-26 12:23:39 -0700

    Added and #included libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
 #include <Arduino.h>
+#include <TinyGPS++.h>
+#include <SparkFun_SGP40_Arduino_Library.h>
+#include <HttpClient.h>
+#include <WiFi.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include <TFT_eSPI.h>
 
 void setup() {
   // put your setup code here, to run once:
