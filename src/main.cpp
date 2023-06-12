5f5ffdfb3731c30d4801f219f47afe4549843211
johndr1 <johndr1@uci.edu>
2023-06-12 16:56:57 -0700

    Finished w/o GPS

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,20 @@
 
 #include <TFT_eSPI.h>
 
+TFT_eSPI tft = TFT_eSPI();
+
 char ssid[50];  // network SSID name
 char pass[50];  // network password
 
 char apiKey[50]; // weather api key
-float latitude; // latitude for weather api
-float longitude; // longitude for weather api
 
 Adafruit_SGP30 sgp;
 
-const int AQ_BUTTON = 0;
-const int WEATHER_BUTTON = 35;
+const int AQ_BUTTON = 0;        // Initialize Air Quality Button Pin
+const int WEATHER_BUTTON = 35;  // Initialize Weather Button Pin
+const int OFF_BUTTON = 17;      // Initialize Off Button Pin
+const int BUZZER_PIN = 27;      // Initialize Buzzer Pin
+const int BUZZER_BUTTON = 2;    // FOR DEMO PURPOSES ONLY Initialize Buzzer Button Pin
 
 void nvs_access() {
   // Initialize NVS
@@ -99,13 +102,142 @@ String httpGET(const char* hostName) {
   return body;
 }
 
-void getWeatherData() {
-  // will use GPS to get lat and long, but for now just hardcoded
-  latitude = 33.644510345086914;
-  longitude = -117.82569552931159;
+void alertUser(int eCO2) {  // alerts user until button press, then displays eCO2 level
+  uint8_t count = 0;
+  while(digitalRead(OFF_BUTTON)) {
+    if(count%2 == 0) {
+      tft.fillScreen(TFT_RED);
+      tone(BUZZER_PIN, 262);
+    }
+    else {
+      tft.fillScreen(TFT_BLACK);
+      noTone(BUZZER_PIN);
+    }
+    delay(250);
+  }
+  noTone(BUZZER_PIN);
 
+  tft.setCursor(0,0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextFont(2);
+  tft.setTextSize(1);
+  tft.println("Unhealthy eCO2\nLevels Detected!");
+  tft.print("Measured: ");
+  tft.setTextColor(TFT_RED);
+  tft.print(eCO2); tft.println(" ppm");
+  tft.setTextColor(TFT_WHITE);
+  tft.println("Recommend Wearing Mask");
+  delay(1000);
+
+  while(digitalRead(OFF_BUTTON));
+  tft.fillScreen(TFT_BLACK);
+}
+
+int geteCO2() { // fetches eCO2 data from sensor
+  if (!sgp.IAQmeasure()) {
+    Serial.println("Measurement failed");
+    return 0;
+  }
+
+  return sgp.eCO2;
+}
+
+void getAQData() {  // displays air quality data
+  tft.setTextDatum(TC_DATUM);
+  tft.setTextFont(2);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString("Fetching\nData", tft.width()/2, tft.height()/2);
+
+  // get data from sensor
+  if (!sgp.IAQmeasure()) {
+    tft.fillScreen(TFT_RED);
+    tft.setTextColor(TFT_BLACK);
+    tft.drawString("Measurement\nFailed", tft.width()/2, tft.height()/2);
+    Serial.println("Measurement failed");
+    delay(5000);
+    tft.fillScreen(TFT_BLACK);
+    return;
+  }
+
+  // print TVOC and eCO2 data to Serial
+  Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
+  Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
+
+  // get raw data from sensor
+  if (!sgp.IAQmeasureRaw()) {
+    tft.fillScreen(TFT_RED);
+    tft.setTextColor(TFT_BLACK);
+    tft.drawString("Raw\nMeasurement\nFailed", tft.width()/2, tft.height()/2);
+    Serial.println("Raw Measurement failed");
+    delay(5000);
+    tft.fillScreen(TFT_BLACK);
+    return;
+  }
+
+  // print raw data to Serial
+  Serial.print("Raw H2 "); Serial.print(sgp.rawH2); Serial.print(" \t");
+  Serial.print("Raw Ethanol "); Serial.print(sgp.rawEthanol); Serial.println("");
+
+  // fill background
+  tft.fillScreen(TFT_SKYBLUE);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextFont(2);
+
+  char buffer[10];
+
+  // print TVOC data from sensor
+  sprintf(buffer, "%d ppb", sgp.TVOC);
+  tft.setTextSize(1);
+  tft.drawString("TVOC:", 5, 5);
+  tft.setTextDatum(BC_DATUM);
+  tft.setTextSize(2);
+  tft.drawString(String(buffer), tft.width()/2, tft.height()/4 - 5);
+
+  // print eCO2 data from sensor
+  sprintf(buffer, "%d ppm", sgp.eCO2);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextSize(1);
+  tft.drawString("eCO2:", 5, tft.height()/4);
+  tft.setTextDatum(BC_DATUM);
+  tft.setTextSize(2);
+  tft.drawString(String(buffer), tft.width()/2, tft.height()/2 - 7);
+
+  // print raw H2 data from sensor
+  sprintf(buffer, "%d", sgp.rawH2);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextSize(1);
+  tft.drawString("Raw H2:", 5, tft.height()/2);
+  tft.setTextDatum(BC_DATUM);
+  tft.setTextSize(2);
+  tft.drawString(String(buffer), tft.width()/2, 3 * (tft.height()/4) - 7);
+
+  // print raw Ethanol data from sensor
+  sprintf(buffer, "%d", sgp.rawEthanol);
+  tft.setTextDatum(TL_DATUM);
+  tft.setTextSize(1);
+  tft.drawString("Raw Ethanol:", 5, 3 * (tft.height()/4));
+  tft.setTextDatum(BC_DATUM);
+  tft.setTextSize(2);
+  tft.drawString(String(buffer), tft.width()/2, tft.height()-7);
+}
+
+void getWeatherData() { // displays weather data
+  tft.setTextDatum(TC_DATUM);
+  tft.setTextFont(2);
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString("Fetching\nData", tft.width()/2, tft.height()/2);
+
+  // Using Irvine city ID for API call
+  uint32_t cityID = 5359777;
+
+  // fetch weather data
   char apiPath[150];
-  sprintf(apiPath, "https://api.openweathermap.org/data/2.5/weather?lat=%.04f&lon=%.04f&appid=%s&units=imperial", latitude, longitude, apiKey);
+  sprintf(apiPath, "https://api.openweathermap.org/data/2.5/weather?id=%d&appid=%s&units=imperial", cityID, apiKey);
 
   String apiResponse;
   if(WiFi.status() == WL_CONNECTED) {
@@ -114,22 +246,78 @@ void getWeatherData() {
     JSONVar data = JSON.parse(apiResponse);
 
     if(JSON.typeof(data) == "undefined") {
+      tft.fillScreen(TFT_RED);
+      tft.setTextColor(TFT_BLACK);
+      tft.drawString("Parsing\nData\nFailed", tft.width()/2, tft.height()/2);
       Serial.println("Parsing input failed.");
+      delay(5000);
+      tft.fillScreen(TFT_BLACK);
       return;
     }
 
+    // print weather data to Serial
     Serial.print("Description: "); Serial.println(data["weather"][0]["main"]);
     Serial.print("Temperature: "); Serial.print(data["main"]["temp"]); Serial.println(" degrees F");
     Serial.print("Pressure: "); Serial.print(data["main"]["pressure"]); Serial.println(" hPa");
     Serial.print("Humidity: "); Serial.print(data["main"]["humidity"]); Serial.println("%");
     Serial.print("Wind Speed: "); Serial.print(data["wind"]["speed"]); Serial.println(" mph");
+
+    String description = data["weather"][0]["main"];
+
+    char buffer[13];
+
+    // fill background
+    tft.fillScreen(TFT_SKYBLUE);
+    tft.setTextDatum(TC_DATUM);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextFont(2);
+
+    // print description of weather from API
+    tft.setTextSize(2);
+    tft.drawString(description, tft.width()/2, 10);
+
+    // draw line
+    tft.drawLine(0, 52, tft.width(), 52, TFT_WHITE);
+
+    // print temperature in degrees fahrenheit from API
+    sprintf(buffer, "%d", static_cast<int>(data["main"]["temp"]));
+    tft.setTextSize(4);
+    tft.drawString(String(buffer), tft.width()/2, 60);
+    tft.setTextSize(1);
+    tft.drawString("degrees F", tft.width()/2, 110);
+
+    // print air pressure in hPa from API
+    sprintf(buffer, "%d", static_cast<int>(data["main"]["pressure"]));
+    tft.setTextDatum(TL_DATUM);
+    tft.setTextSize(2);
+    tft.drawString(String(buffer), 15, 130);
+    tft.setTextSize(1);
+    tft.drawString("hPa", tft.width()/2 + 20, 140);
+
+    // print humidity percentage from API
+    sprintf(buffer, "%d%", static_cast<int>(data["main"]["humidity"]));
+    tft.setTextSize(2);
+    tft.drawString(String(buffer), 15, 160);
+    tft.setTextSize(1);
+    tft.drawString("humidity", tft.width()/2 + 5, 170);
+
+    // print wind speed in mph from API
+    sprintf(buffer, "%d", static_cast<int>(data["wind"]["speed"]));
+    tft.setTextSize(2);
+    tft.drawString(String(buffer), 15, 190);
+    tft.setTextSize(1);
+    tft.drawString("mph wind", tft.width()/2 - 15, 200);
   }
 }
 
 void setup() {
   Serial.begin(9600);
+  // set up I/O pins
   pinMode(AQ_BUTTON, INPUT_PULLUP);
   pinMode(WEATHER_BUTTON, INPUT_PULLUP);
+  pinMode(OFF_BUTTON, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUZZER_BUTTON, INPUT_PULLUP);
   delay(1000);
   
   // Retrieve SSID/PASSWD/apiKey from flash
@@ -151,60 +339,39 @@ void setup() {
 
   Serial.println("");
   Serial.println("WiFi connected\n");
-  //Serial.println("IP address: ");
-  //Serial.println(WiFi.localIP());
-  //Serial.println("MAC address: ");
-  //Serial.println(WiFi.macAddress());
 
   if (!sgp.begin()) {
     Serial.println("Sensor not found.");
     while(1);
   }
 
-  Serial.println("Application Starting Now\n");
+  // initialize screen
+  tft.init();
+  tft.setRotation(4);
+  tft.fillScreen(TFT_BLACK);
 }
 
 void loop() {
-  if (!digitalRead(AQ_BUTTON)) {
-    Serial.println("Fetching Air Quality Data...");
-
-    if (!sgp.IAQmeasure()) {
-      Serial.println("Measurement failed");
-      return;
-    }
-
-    Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
-    Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
-
-    if (!sgp.IAQmeasureRaw()) {
-      Serial.println("Raw Measurement failed");
-      return;
-    }
-
-    Serial.print("Raw H2 "); Serial.print(sgp.rawH2); Serial.print(" \t");
-    Serial.print("Raw Ethanol "); Serial.print(sgp.rawEthanol); Serial.println("");
-
-    delay(1000);
-
-    while(!digitalRead(AQ_BUTTON)) {
-      delay(250);
-    }
-
-    Serial.println("Returning to Home\n");
+  if (!digitalRead(AQ_BUTTON)) {  // displays air quality data on button press
+    getAQData();
   }
 
-  if (!digitalRead(WEATHER_BUTTON)) {
-    Serial.println("Fetching Local Weather Data...");
-
+  if (!digitalRead(WEATHER_BUTTON)) { // displays weather data on button press
     getWeatherData();
+  }
 
-    delay(1000);
+  if (!digitalRead(OFF_BUTTON)) { // turns display off on button press
+    tft.fillScreen(TFT_BLACK);
+  }
 
-    while(!digitalRead(WEATHER_BUTTON)) {
-      delay(250);
-    }
+  int eCO2 = geteCO2();
 
-    Serial.println("Returning to Home\n");
+  if (eCO2 >= 600) {  // triggers alert if eCO2 levels are above threshold
+    alertUser(eCO2);
+  }
+
+  if (!digitalRead(BUZZER_BUTTON)) {  // Manually triggers alert on button press (DEMO PURPOSES ONLY)
+    alertUser(600);
   }
 
   delay(100);
